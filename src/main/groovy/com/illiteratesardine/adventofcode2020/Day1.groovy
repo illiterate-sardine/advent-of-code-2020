3e631d2112bd3a8132567f55b19f532d2ef7f3cb
Peter_Jones <peteraxeljones@gmail.com>
2020-12-02 19:15:32 -0600

    added getExpenseReportList

--- src/main/groovy/com/illiteratesardine/adventofcode2020/Day1.groovy
+++ src/main/groovy/com/illiteratesardine/adventofcode2020/Day1.groovy
@@ -5,5 +5,15 @@ class Day1 {
     Integer calculatePart1() {
         4
     }
+    
+    List<Integer> getExpenseReportList() {
+        Scanner scanner = new Scanner(new File('src/main/resources/day1'))
+        List<Integer> expenses = new ArrayList<Integer>()
+        while (scanner.hasNextInt()) {
+            expenses.add(scanner.nextInt())
+            }
+            
+            expenses
+            }
 
 }
